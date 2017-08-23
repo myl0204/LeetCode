43da5afc20d2ee196a7b5a4b456e8c6a72044910
myl <yunliangmiao@gmail.com>
2017-08-23 19:42:51 +0800

    upload correct code

--- ac/16. 3Sum Closest.js
+++ ac/16. 3Sum Closest.js
@@ -1,1 +1,29 @@
-16. 3Sum Closest.js
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+	nums.sort((a,b)=>a-b);
+	var length = nums.length,result = nums[0] + nums[1] + nums[length-1];
+	for(var i = 0; i < length-2; i++) {
+		while(nums[i] == nums[i-1]) {
+			i++;
+		}
+		var l = i + 1,r = length - 1;
+		while(l<r) {
+            var cur = nums[i] + nums[l] + nums[r] ;
+			if(nums[i] + nums[l] + nums[r] == target ) {
+				result = target;
+				return result;
+			}else if(nums[i] + nums[l] + nums[r] - target < 0){
+				result = Math.abs(result-target) > Math.abs(cur-target) ? cur:result;
+				l++;
+			}else{
+				result = Math.abs(result-target) > Math.abs(cur-target) ? cur:result;
+				r--;
+			}
+		}
+	}
+	return result;
+};
